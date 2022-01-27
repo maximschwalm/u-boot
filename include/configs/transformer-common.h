b5ac60221a206658c75dcfdb9456f80315a4ad26
Svyatoslav Ryhel <clamor95@gmail.com>
2022-01-27 19:52:55 +0200

    board: asus: transformer: add ASUS Transformer T30 family support
    The Asus Transformer T30 family are 2-in-1 detachable tablets
    and AiO developed by Asus that run the Android operating system
    (TF600T runs Windows RT and P1801-T runs Android and Windows).
    The T30 Transformers feature a 10.1-inch display (apart P1801-T),
    an Nvidia Tegra 3 quad-core chip, 1/2 GB of RAM, and 16/32 GB of storage.
    Transformers board derives from Nvidia Cardhu development board.
    
    This patch brings support for 7 known Transformer devices:
    - ASUS Transformer Prime TF201
    - ASUS Transformer Pad TF300T/TF300TG/TF300TL
    - ASUS VivoTab RT TF600T (Windows RT based)
    - ASUS Transformer Infinity TF700T
    - ASUS Transformer AiO P1801-T
    
    Tested-by: Andreas Westman Dorcsak <hedmoo@yahoo.com> # all devices
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- include/configs/transformer-common.h
+++ include/configs/transformer-common.h
@@ -31,12 +31,46 @@
 		"else echo Reading bootloader failed;" \
 			"pause 'Press ANY key to return to bootmenu...'; bootmenu; fi\0"
 
+#define TRANSFORMER_FLASH_SPI \
+	"update_spi=sf probe 0:1;" \
+		"echo Dumping current SPI flash content ...;" \
+		"sf read ${kernel_addr_r} 0x0 ${spi_size};" \
+		"if fatwrite mmc 1:1 ${kernel_addr_r} spi-flash-backup.bin ${spi_size};" \
+		"then echo SPI flash content was successfully written into spi-flash-backup.bin;" \
+			"echo Reading SPI flash binary;" \
+			"if load mmc 1:1 ${kernel_addr_r} repart-block.bin;" \
+			"then echo Writing bootloader into SPI flash;" \
+				"sf probe 0:1;" \
+				"sf update ${kernel_addr_r} 0x0 ${spi_size};" \
+				"reset;" \
+			"else echo Preparing RAM;" \
+				"mw ${kernel_addr_r} 0 ${boot_block_size_r};" \
+				"mw ${ramdisk_addr_r} 0 ${boot_block_size_r};" \
+				"echo Reading BCT;" \
+				"sf read ${kernel_addr_r} 0x0 ${boot_block_size_r};" \
+				"echo Reading bootloader;" \
+				"if load mmc 1:1 ${ramdisk_addr_r} ${bootloader_file};" \
+				"then echo Calculating bootloader size;" \
+					"size mmc 1:1 ${bootloader_file};" \
+					"ebtupdate ${kernel_addr_r} ${ramdisk_addr_r} ${filesize};" \
+					"echo Writing bootloader into SPI flash;" \
+					"sf probe 0:1;" \
+					"sf update ${kernel_addr_r} 0x0 ${boot_block_size_r};" \
+					"sf update ${ramdisk_addr_r} ${boot_block_size_r} ${boot_block_size_r};" \
+					"echo Bootloader written successfully; reset;" \
+				"else echo Reading bootloader failed;" \
+					"poweroff; fi;" \
+			"fi;" \
+		"else echo SPI flash backup FAILED! Aborting ...;" \
+			"poweroff; fi\0"
+
 #define TRANSFORMER_REFRESH_USB \
 	"refresh_usb=usb start; usb reset; usb tree; usb info;" \
 		"pause 'Press ANY key to return to bootmenu...'; bootmenu\0"
 
 #define TRANSFORMER_BOOTMENU \
 	TRANSFORMER_FLASH_UBOOT \
+	TRANSFORMER_FLASH_SPI \
 	TRANSFORMER_REFRESH_USB \
 	"bootmenu_0=mount internal storage=usb start && ums 0 mmc 0; bootmenu\0" \
 	"bootmenu_1=mount external storage=usb start && ums 0 mmc 1; bootmenu\0" \
